09a11f74950c249428447253b84d22d23ad5c9cf
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-06-15 20:10:54 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *main - this is the main function
- *Return: always 0
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
