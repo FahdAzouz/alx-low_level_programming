69378297e764f69d71a61eae26ca2a07f0130165
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-06-15 20:03:06 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#include  <stdio.h>
-#include <string.h>
-
+include <stdio.h>
+/**
+ *main - this is the main function
+ *Return: always 0
+ */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
