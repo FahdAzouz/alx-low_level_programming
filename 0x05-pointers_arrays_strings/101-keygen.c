88f9c1f7a86f8034faeb405bd6c6dcc816708664
FahdAzouz <fahdstudies00@gmail.com>
2023-06-27 03:08:52 +0100

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, s, n;
+
+	s = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		s += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - s) - '0' < 78)
+		{
+			n = 2772 - s - '0';
+			s += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
