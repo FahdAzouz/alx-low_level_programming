8654573f65a4dddea212f3e431435f4f4e819e50
FahdAzouz <fahdstudies00@gmail.com>
2023-06-29 19:47:55 +0100

    strcmpr

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * function that compares two strings.
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: s1[i] - s2[i]
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
