b814a1b222711db530a7fd13ae498f4f15baf5f7
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-07-03 19:22:08 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,7 +15,6 @@ void print_chessboard(char (*a)[8])
                 {
                         _putchar(a[i][j]);
                 }
-                if (i != 7)
-                        _putchar('\n');
+                _putchar('\n');
         }
 }
