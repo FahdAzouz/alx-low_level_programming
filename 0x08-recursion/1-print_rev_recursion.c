f54cdb5e0cfeaf41195e14714c23a207eb6b20a2
FahdAzouz <fahdstudies00@gmail.com>
2023-07-05 16:55:36 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,9 +9,9 @@
 
 void _print_rev_recursion(char *s)
 {
-        if (*s)
-        {
-                _print_rev_recursion(s + 1);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-        }
+	}
 }
