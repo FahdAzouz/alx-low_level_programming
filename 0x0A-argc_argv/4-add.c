39432c40d4e86b1acefbf28c67ed8d97bd073666
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-07-07 21:49:04 +0100

    Add files via upload

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - entry point
+ * @argc: character
+ * @argv: character
+ *
+ * Description: -
+ * Return: -
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i, j;
+
+	sum = 0;
+	if (argc != 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j < (int)strlen(argv[i]); j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
