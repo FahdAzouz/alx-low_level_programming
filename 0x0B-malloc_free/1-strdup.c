5898d462186674f60309739ee8ea500739f3eaf1
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-07-10 18:12:39 +0100

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,13 @@ char *_strdup(char *str)
 {
 	char *copy;
 	int i, len = 0;
-	
-	if(str == NULL)
-                return (NULL);
-	while(str[len] != '\0')
+
+	if (str == NULL)
+        return (NULL);
+	while (str[len] != '\0')
 		len++;
-	copy = malloc(sizeof(char) * len);
-	if(copy == NULL)
+	copy = malloc((sizeof(char) * len) + 1);
+	if (copy == NULL)
 		return (NULL);
 	for (i = 0 ; i < len ; i++)
 		copy[i] = str[i];
