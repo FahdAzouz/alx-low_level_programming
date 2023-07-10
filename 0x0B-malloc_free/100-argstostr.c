b33d63618daa903224227948428febacb01a7400
FahdAzouz <fahdstudies00@gmail.com>
2023-07-10 18:57:29 +0100

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,28 +11,28 @@
 
 char *argstostr(int ac, char **av)
 {
-    char *concat;
-    int i, j, k = 0, len = 0;
+	char *concat;
+	int i, j, k = 0, len = 0;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
-    for (i = 0 ; i < ac ; i++)
-    {
-        for (j = 0 ; av[i][j] != '\0' ; j++)
-            len++;
-    }
-    concat = malloc((sizeof(char) * len) + ac + 1);
-    if (concat == NULL)
-        return (NULL);
-    for (i = 0 ; i < ac ; i++)
-    {
-        for (j = 0 ; av[i][j] != '\0' ; j++)
-        {
-            concat[k] = av[i][j];
-            k++;
-        }
-        concat[k] = '\n';
-        k++;
-    }
-    return (concat);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0 ; i < ac ; i++)
+	{
+		for (j = 0 ; av[i][j] != '\0' ; j++)
+			len++;
+	}
+	concat = malloc((sizeof(char) * len) + ac + 1);
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0 ; i < ac ; i++)
+	{
+		for (j = 0 ; av[i][j] != '\0' ; j++)
+		{
+			concat[k] = av[i][j];
+			k++;
+		}
+		concat[k] = '\n';
+		k++;
+	}
+	return (concat);
 }
