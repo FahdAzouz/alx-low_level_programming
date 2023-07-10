a083e2425fda16cf9f7307e3c750201a805860c5
FahdAzouz <fahdstudies00@gmail.com>
2023-07-10 18:24:53 +0100

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 
 /**
  * str_concat - function that concatenates two strings.
- * 
+ *
  * @s1: string 1
  * @s2: string 2
  * Return: char*
@@ -11,23 +11,23 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    int i, j, len1 = 0, len2 = 0;
+	char *concat;
+	int i, j, len1 = 0, len2 = 0;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
-    concat = malloc((sizeof(char) * (len1 + len2)) + 1);
-    if (concat == NULL)
-        return (NULL);
-    for (i = 0 ; i < len1 ; i++)
-        concat[i] = s1[i];
-    for (j = 0 ; j < len2 ; j++)
-        concat[i + j] = s2[j];
-    return (concat);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+	concat = malloc((sizeof(char) * (len1 + len2)) + 1);
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0 ; i < len1 ; i++)
+		concat[i] = s1[i];
+	for (j = 0 ; j < len2 ; j++)
+		concat[i + j] = s2[j];
+	return (concat);
 }
