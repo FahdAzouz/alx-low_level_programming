9b12bd85066d6336a5ff3b33e2ec6a58c0a9d8e3
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-07-17 23:33:37 +0100

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return (EXIT_SUCCESS);
+}
