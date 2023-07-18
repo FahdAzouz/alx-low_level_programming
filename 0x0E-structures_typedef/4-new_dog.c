c9e8ca445ccc03817eb440f5616ede10b490fee0
FahdAzouz <fahdstudies00@gmail.com>
2023-07-18 03:10:34 +0100

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,35 +13,35 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    int i, j, k, l;
+	dog_t *new_dog;
+	int i, j, k, l;
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-    for (i = 0; name[i]; i++)
-        ;
-    i++;
-    new_dog->name = malloc(i * sizeof(char));
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-    for (j = 0; owner[j]; j++)
-        ;
-    j++;
-    new_dog->owner = malloc(j * sizeof(char));
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
-    for (k = 0; k < i; k++)
-        new_dog->name[k] = name[k];
-    new_dog->age = age;
-    for (l = 0; l < j; l++)
-        new_dog->owner[l] = owner[l];
-    return (new_dog);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		;
+	i++;
+	new_dog->name = malloc(i * sizeof(char));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	for (j = 0; owner[j]; j++)
+		;
+	j++;
+	new_dog->owner = malloc(j * sizeof(char));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		new_dog->name[k] = name[k];
+	new_dog->age = age;
+	for (l = 0; l < j; l++)
+		new_dog->owner[l] = owner[l];
+	return (new_dog);
 }
