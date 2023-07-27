54b2a67b7088f47bd0fa47e8b1fb62d386f205d7
FahdAzouz <fahdstudies00@gmail.com>
2023-07-27 20:24:26 +0100

    Add node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -33,12 +33,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new == NULL)
 	{
 		free(new);
+		return (NULL);
 	}
 	new->str = strdup(str);
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	new->len = i;
+	new->len = _strlen(str);
 	new->next = NULL;
 	if (*head == NULL)
 	{
