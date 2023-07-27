ff792e7350b363e92f8f06a8027ba1defe93eb3b
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-07-27 18:58:32 +0100

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,4 +8,15 @@
  */
 
 size_t print_list(const list_t *h){
+    size_t i = 0;
 
+    while (h != NULL){
+        if (str == NULL)
+            printf("[0] (nil)");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+            h = h->next;
+            i++;
+    }
+    return (i);
+}
