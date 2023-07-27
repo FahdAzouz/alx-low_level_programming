562f792a96e3d4e53b219bdff771bdb2e3feab16
FahdAzouz <fahdstudies00@gmail.com>
2023-07-27 18:47:12 +0100

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of
+ * a list_t list
+ * @h: list to print
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h){
+
