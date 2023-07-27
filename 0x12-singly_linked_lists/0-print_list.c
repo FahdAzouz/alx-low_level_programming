9b81ef296ebca91365ebdbcd156f313af4b8c6e9
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-07-27 19:31:54 +0100

    Print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@
 
 size_t	print_list(const list_t *h)
 {
-	int i = 0;
+	int nu = 0;
 
 	while (h != NULL)
 	{
@@ -16,7 +16,7 @@ size_t	print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		nu++;
 	}
-	return (i);
+	return (nu);
 }
