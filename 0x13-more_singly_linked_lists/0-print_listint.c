676142d01c5a7e8fe2de3bdf263ec9d5d1739312
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-07-31 23:27:42 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count;
-count = 0 ;
+	count = 0 ;
 	if (h == NULL)
 		return (0);
 
@@ -18,6 +18,8 @@ count = 0 ;
 	{
 		printf("%d\n", h->n);
 		count++;
+		if (h->next == NULL)
+			break;
 		h = h->next;
 	}
 	return (count);
