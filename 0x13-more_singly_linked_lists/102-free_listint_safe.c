786223c7b6ade09adca0628d943471c71b6fd53e
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-08-02 00:05:40 +0100

    ff

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function that frees a linked list
+ * @h: linked list
+ * Return: number of nodes in the list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+    size_t count = 0;
+    listint_t *temp = *h, *temp2;
+
+    if (h == NULL)
+        return (0);
+
+    while (temp != NULL)
+    {
+        temp2 = temp->next;
+        free(temp);
+        count++;
+        if (temp2 >= temp)
+            break;
+        temp = temp2;
+    }
+
+    *h = NULL;
+
+    return (count);
+}
