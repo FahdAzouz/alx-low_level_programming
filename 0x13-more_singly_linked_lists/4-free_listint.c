f21458193144307411a39350d047a26550b607f1
FahdAzouz <fahdstudies00@gmail.com>
2023-08-01 20:12:36 +0100

    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,15 +8,16 @@
 
 void free_listint(listint_t *head)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    if (head == NULL)
-        return;
+	if (head == NULL)
+		return;
 
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
+
