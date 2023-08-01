204486d99b464dcccd912c3a974cb2a1e4ba706b
FahdAzouz <fahdstudies00@gmail.com>
2023-08-02 00:08:19 +0100

    Free (safe)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,23 +8,24 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-    size_t count = 0;
-    listint_t *temp = *h, *temp2;
+	size_t count = 0;
+	listint_t *temp = *h, *temp2;
 
-    if (h == NULL)
-        return (0);
+	if (h == NULL)
+		return (0);
 
-    while (temp != NULL)
-    {
-        temp2 = temp->next;
-        free(temp);
-        count++;
-        if (temp2 >= temp)
-            break;
-        temp = temp2;
-    }
+	while (temp != NULL)
+	{
+		temp2 = temp->next;
+		free(temp);
+		count++;
+		if (temp2 >= temp)
+			break;
+		temp = temp2;
+	}
 
-    *h = NULL;
+	*h = NULL;
 
-    return (count);
+	return (count);
 }
+
