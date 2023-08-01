7b508b270b72ba5ab84f1e96dae7a77d7c2629a6
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-08-01 20:47:42 +0100

    Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of all the data (n) of a
+ * listint_t linked list
+ * @head: linked list
+ * Return: sum of all the data (n) of a listint_t linked list
+ */
+
+int sum_listint(listint_t *head)
+{
+    listint_t *temp;
+    int sum;
+
+    if (head == NULL)
+        return (0);
+
+    temp = head;
+    sum = 0;
+    while (temp != NULL)
+    {
+        sum += temp->n;
+        temp = temp->next;
+    }
+
+    return (sum);
+}
