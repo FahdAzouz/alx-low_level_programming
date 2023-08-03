4f90962ba6f66de83061b956dd03d0a773058d12
FahdAzouz <fahdstudies00@gmail.com>
2023-08-03 12:17:35 +0100

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,27 +8,28 @@
 
 void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1;
-    int flag = 0;
+	unsigned long int mask = 1;
+	int flag = 0;
 
-    mask <<= 63;
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-    while (mask > 0)
-    {
-        if ((n & mask) == 0)
-        {
-            if (flag == 1)
-                _putchar('0');
-        }
-        else
-        {
-            _putchar('1');
-            flag = 1;
-        }
-        mask >>= 1;
-    }
+	mask <<= 63;
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+		{
+			if (flag == 1)
+				_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		mask >>= 1;
+	}
 }
+
