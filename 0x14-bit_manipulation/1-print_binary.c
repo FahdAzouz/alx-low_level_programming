edf48299f6487d1b734b563b27e5fc4f0e58dd4a
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-08-03 12:14:32 +0100

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,23 +7,31 @@
  */
 
 void print_binary(unsigned long int n)
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1;
-    int i = 0;
+	unsigned long int tmp;
+	int shifts;
 
-    while (mask < n)
-    {
-        mask <<= 1;
-        i++;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-    while (i >= 0)
-    {
-        if (n & mask)
-            _putchar('1');
-        else
-            _putchar('0');
-        mask >>= 1;
-        i--;
-    }
+	for (tmp = n, shifts = 0; (tmp >>= 1) > 0; shifts++)
+		;
+
+	while (shifts >= 0)
+	{
+		if ((n >> shifts) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+		shifts--;
+	}
 }
