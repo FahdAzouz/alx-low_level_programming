328ea4f7ea6e83d4058359b061fa3e8cdd31b184
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-08-07 22:36:25 +0100

    hh

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,8 @@
  * The created file must have those permissions: rw-------. If the file
  * already exists, do not change the permissions.
  * if the file already exists, truncate it
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
@@ -12,7 +14,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd, written, i;
+    int fd, written, len = 0;
 
     if (filename == NULL)
         return (-1);
@@ -24,11 +26,10 @@ int create_file(const char *filename, char *text_content)
 
     if (text_content != NULL)
     {
-        for (i = 0; text_content[i] != '\0'; i++)
-            ;
+        while (text_content[len])
+            len++;
 
-        written = write(fd, text_content, i);
-
+        written = write(fd, text_content, len);
         if (written < 0)
             return (-1);
     }
