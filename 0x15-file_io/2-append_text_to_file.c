bf72d8c30beb150a0b0d504aa56582714384df7b
FahdAzouz <fahdstudies00@gmail.com>
2023-08-07 23:12:19 +0100

    io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,6 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * Do not create the file if it does not exist
- * 
  * @filename: name of the file
  * @text_content: NULL terminated string to add at the end of the file
  * Return: 1 on success, -1 on failure
@@ -11,27 +10,28 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, written, len = 0;
+	int fd, written, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    if (fd < 0)
-        return (-1);
+	if (fd < 0)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len] != '\0')
-            len++;
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
 
-        written = write(fd, text_content, len);
-        if (written != len)
-            return (-1);
-    }
+		written = write(fd, text_content, len);
+		if (written != len)
+			return (-1);
+	}
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
+
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,8 +3,6 @@
 /**
  * main - copies the content of a file to another file
  * if file_to already exists, truncate it
- * if file_from does not exist, or if you can not read it, exit with code 98
- * and print Error: Can't read from file NAME_OF_THE_FILE, followed by a new line,
  * on the POSIX standard error
  * @argc: number of arguments
  * @argv: array of arguments
@@ -13,37 +11,40 @@
 
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to, readed, written;
-    char buffer[1024];
+	int fd_from, fd_to, readed, written;
+	char buffer[1024];
 
-    if (argc != 3)
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from < 0)
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
 
-    fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (fd_to < 0)
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to < 0)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
-    readed = read(fd_from, buffer, 1024);
-    while (readed > 0)
-    {
-        written = write(fd_to, buffer, readed);
-        if (written < 0)
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-        readed = read(fd_from, buffer, 1024);
-    }
+	readed = read(fd_from, buffer, 1024);
+	while (readed > 0)
+	{
+		written = write(fd_to, buffer, readed);
+		if (written < 0)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		readed = read(fd_from, buffer, 1024);
+	}
 
-    if (readed < 0)
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (readed < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %\n",
+				argv[1]), exit(98);
 
-    if (close(fd_from) < 0)
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_from) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 
-    if (close(fd_to) < 0)
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	if (close(fd_to) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 
-    return (0);
+	return (0);
 }
+
