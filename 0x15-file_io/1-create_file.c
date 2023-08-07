17e2e7c340185be0449a0ee0dcef8ce17d3a6fc7
FahdAzouz <fahdstudies00@gmail.com>
2023-08-07 22:51:17 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,27 +14,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd, written, len = 0;
+	int fd, written, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-    if (fd < 0)
-        return (-1);
+	if (fd < 0)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len] != '\0')
-            len++;
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
 
-        written = write(fd, text_content, len);
-        if (written != len)
-            return (-1);
-    }
+		written = write(fd, text_content, len);
+		if (written != len)
+			return (-1);
+	}
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
+
