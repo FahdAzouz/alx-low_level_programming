c0205fc72540239f25fe89e06b002aaaf3d2b242
FahdAzouz <fahdstudies00@gmail.com>
2023-08-07 21:51:51 +0100

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,41 +9,43 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    int readed, printed;
-    char *buffer;
+	int fd;
+	int readed, printed;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    readed = read(fd, buffer, letters);
-    if (readed < 0)
-    {
-        free(buffer);
-        return (0);
-    }
-    buffer[readed] = '\0';
-    close(fd);
+	readed = read(fd, buffer, letters);
+	if (readed < 0)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    printed = write(STDOUT_FILENO, buffer, readed);
-    if (printed < 0)
-    {
-        free(buffer);
-        return (0);
-    }
+	buffer[readed] = '\0';
+	close(fd);
 
-    free(buffer);
-    
-    return (printed);
+	printed = write(STDOUT_FILENO, buffer, readed);
+	if (printed < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+
+	return (printed);
 }
+
