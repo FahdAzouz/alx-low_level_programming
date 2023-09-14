352051313d8bc4d040bd17017e2e0648a59dc859
FahdAzouz <fahdstudies00@gmail.com>
2023-09-14 15:02:39 +0100

    Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,13 +11,13 @@ int sum_dlistint(dlistint_t *head)
 	unsigned int i = 0;
 
 	if (!head)
-		return (NULL);
+		return (0);
 	if (head == NULL)
 		return (0);
 
-	while (head)
+	while (head != NULL)
 	{
-		i += head->n;
+		i = i + head->n;
 		head = head->next;
 	}
 
