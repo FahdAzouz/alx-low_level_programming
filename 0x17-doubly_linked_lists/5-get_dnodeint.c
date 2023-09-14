dcffa726f0801c3a631f5271e452fad364143db6
FahdAzouz <fahdstudies00@gmail.com>
2023-09-14 14:52:05 +0100

    Get node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	while (i < index)
+	while (head && i < index)
 	{
 		node = node->next;
 		i++;
