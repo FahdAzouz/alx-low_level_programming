e18d8ffc40642d392a44ce96891f32cf4107d154
FahdAzouz <fahdstudies00@gmail.com>
2023-10-06 16:50:31 +0100

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-     // Creates a new HashTable.
-     hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
-     if (table == NULL)
-         return NULL;
-     table->size = size;
-     if (size == 0)
-         return table;
-     table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t*));
-     if (table->array == NULL)
-         return NULL;
-     for (int i = 0; i < table->size; i++)
-         table->array[i] = NULL;
+	hash_table_t *ht;
 
-     return table;
+	if (!size)
+		return (NULL);
+
+	ht = calloc(1, sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
 }
