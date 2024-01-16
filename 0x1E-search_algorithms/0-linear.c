4a881a7e4a2f07ce8f325058794e2e345a5ef064
FahdAzouz <fahdstudies00@gmail.com>
2024-01-16 16:57:25 +0100

    style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
@@ -9,6 +8,7 @@
  * @value: value to search in
  * Return: Always EXIT_SUCCESS
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	int i;
